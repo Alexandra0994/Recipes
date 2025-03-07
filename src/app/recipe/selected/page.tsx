31cb559995878f028e91c46de4ef87cc2f69ee77
Alexandra <150593473+Alexandra0994@users.noreply.github.com>
2025-03-07 19:24:04 +0100

    feat:add suspense

--- src/app/recipe/selected/page.tsx
+++ src/app/recipe/selected/page.tsx
@@ -3,9 +3,13 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchRecipeById } from "../../../utils/api";
 import { useParams } from "next/navigation";
 import { Container, Typography, CircularProgress } from "@mui/material";
+import { Suspense } from "react";
 
-export default function RecipePage() {
+function RecipeContent() {
     const { id } = useParams();
+
+    if (!id) return <Typography variant="h6">not found</Typography>;
+
     const { data: recipe, isLoading } = useQuery({
         queryKey: ["recipe", id],
         queryFn: () => fetchRecipeById(id as string),
@@ -13,11 +17,21 @@ export default function RecipePage() {
 
     if (isLoading) return <CircularProgress />;
 
+    if (!recipe) return <Typography variant="h6">Рецепт не найден</Typography>;
+
     return (
         <Container>
             <Typography variant="h4">{recipe.strMeal}</Typography>
             <img src={recipe.strMealThumb} alt={recipe.strMeal} width={300} />
             <Typography variant="body1">{recipe.strInstructions}</Typography>
         </Container>
+    );
+}
+
+export default function RecipePage() {
+    return (
+        <Suspense fallback={<CircularProgress />}>
+            <RecipeContent />
+        </Suspense>
     );
 }
