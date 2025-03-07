d1807b4399f402a3001b2463b962a64f61dfc141
Alexandra <150593473+Alexandra0994@users.noreply.github.com>
2025-03-07 19:28:42 +0100

    feat:fix

--- src/app/recipe/selected/page.tsx
+++ src/app/recipe/selected/page.tsx
@@ -1,28 +1,34 @@
 "use client";
+
 import { useQuery } from "@tanstack/react-query";
-import { fetchRecipeById } from "../../../utils/api";
+import { fetchRecipeById } from "@/utils/api";
 import { useParams } from "next/navigation";
 import { Container, Typography, CircularProgress } from "@mui/material";
 import { Suspense } from "react";
+import Image from "next/image";
 
 function RecipeContent() {
     const { id } = useParams();
-
-    if (!id) return <Typography variant="h6">not found</Typography>;
-
     const { data: recipe, isLoading } = useQuery({
         queryKey: ["recipe", id],
         queryFn: () => fetchRecipeById(id as string),
+        enabled: !!id,
     });
 
+    if (!id) return <Typography variant="h6">Рецепт не найден</Typography>;
     if (isLoading) return <CircularProgress />;
-
     if (!recipe) return <Typography variant="h6">Рецепт не найден</Typography>;
 
     return (
         <Container>
             <Typography variant="h4">{recipe.strMeal}</Typography>
-            <img src={recipe.strMealThumb} alt={recipe.strMeal} width={300} />
+            <Image
+                src={recipe.strMealThumb}
+                alt={recipe.strMeal}
+                width={300}
+                height={300}
+                priority
+            />
             <Typography variant="body1">{recipe.strInstructions}</Typography>
         </Container>
     );
